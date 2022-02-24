d88aea8d270a3cfdbb2bdae342c29cbc3583b3c5
OGDAMS <damilaresamson.ogunsiji@gmail.com>
2022-02-24 07:57:41 +0100

    publish tags added

--- src/ContactServiceProvider.php
+++ src/ContactServiceProvider.php
@@ -17,11 +17,24 @@ class ContactServiceProvider extends ServiceProvider
     {
         $this->loadRoutesFrom(__DIR__.'/routes/web.php'); //load routes
         $this->loadMigrationsFrom(__DIR__.'/database/migrations'); //load migrations files
+        //publish migration with tags //php artisan vendor:publish --tag=migrations
+    $this->publishes([__DIR__.'/database/migrations/' => database_path('migrations')], 'migrations');
+    //publish seeders
+   // $this->publishes([__DIR__.'/database/seeders/' => database_path('seeders')], 'seeders');
+
         $this->loadViewsFrom(__DIR__.'/views', 'contact'); //load views
+        //publish email mark down
+        $this->publishes([
+            __DIR__.'/views' => resource_path('views\contact\email.php'),
+        ], 'email-mark-down');
+        //publish contact email
+        $this->publishes([
+            __DIR__.'/views' => resource_path('views\email.php'),
+        ], 'contact-form');
         $this->mergeConfigFrom(__DIR__.'/config/contact.php', 'contact'); //merge configuration files
         $this->publishes([
             __DIR__.'/config/contact.php' => config_path('contact.php'),
-        ]);  //publish files
+        ], 'config-contact');  //publish files
 
 
     }
